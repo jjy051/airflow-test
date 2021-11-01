5a870194099e6c70b7066ee81afdfefb052aa00c
Jaeyoung <dmos7777@gmail.com>
2021-11-01 23:59:57 +0900

    Create trigger_rule.py

---
+++ dags/trigger_rule.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.utils.task_group import TaskGroup
+
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2021, 1, 1)
+}
+
+with DAG('xcom_dag', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
+    
+    task_1 = BashOperator(
+        task_id='task_1',
+        bash_command='exit 1',
+        do_xcom_push=False
+    )
+
+    task_2 = BashOperator(
+        task_id='task_2',
+        bash_command='exit 1',
+        do_xcom_push=False
+    )
+
+    task_3 = BashOperator(
+        task_id='task_3',
+        bash_command='exit 1',
+        do_xcom_push=False,
+        trigger_rule='one_failed'
+    )
+    
+    [task_1, task_2] >> task_3
